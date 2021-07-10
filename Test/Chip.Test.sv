6a1416c7ba2608730d421ca37c34f469f599d34b
true_mogician <925269713@qq.com>
2021-07-10 14:48:03 +0800

    fix: clearPcOffset

--- Test/Chip.Test.sv
+++ Test/Chip.Test.sv
@@ -10,26 +10,26 @@ package PrintingTime;
 endpackage
 import PrintingTime::*;
 class Configuration;
-	real Period;
-	bit ClearPcOffset;
-	int MaxReplication;
-	bit FileOutput;
-	string FileName;
-	PrintingTimeEnum PrintingTime;
+	real period;
+	bit clearPcOffset;
+	int maxReplication;
+	bit fileOutput;
+	string fileName;
+	PrintingTimeEnum printingTime;
 	function new (
 		input real period = 10,
 		input bit clearPcOffset = '0,
 		input int maxReplication = 8,
 		input bit fileOutput = '1,
 		input string fileName = "output.txt",
-		input PrintingTimeEnum pTime = PrintingTime::AfterExecution
+		input PrintingTimeEnum printingTime = PrintingTime::AfterExecution
 	);
-		this.Period = period;
-		this.ClearPcOffset = clearPcOffset;
-		this.MaxReplication = maxReplication;
-		this.FileOutput = fileOutput;
-		this.FileName = fileName;
-		this.PrintingTime = pTime;
+		this.period = period;
+		this.clearPcOffset = clearPcOffset;
+		this.maxReplication = maxReplication;
+		this.fileOutput = fileOutput;
+		this.fileName = fileName;
+		this.printingTime = printingTime;
 	endfunction
 endclass
 
@@ -42,30 +42,30 @@ module ChipTest;
 	int file, replicationCount = 0;
 	Instr lastInstruction = 'z;
 	initial begin
-		if (conf.FileOutput)
-			file = $fopen(conf.FileName, "w");
+		if (conf.fileOutput)
+			file = $fopen(conf.fileName, "w");
 		enable = '0;
 		reset = '0;
 		clock = '0;
-		#(conf.Period / 2);
+		#(conf.period / 2);
 		reset = '1;
-		#(conf.Period / 2);
+		#(conf.period / 2);
 		reset = '0;
 		enable = '1;
 	end
 	always begin
 		if (~enable)
-			#(conf.Period / 2);
+			#(conf.period / 2);
 		else begin
-			if (conf.PrintingTime == PrintingTime::BeforeExecution) begin
-				#(conf.Period / 2);
+			if (conf.printingTime == PrintingTime::BeforeExecution) begin
+				#(conf.period / 2);
 				clock = ~clock;
 			end
 			if (clock & ~reset) begin
 				if (chip.instr === lastInstruction) begin
 					++replicationCount;
-					if (replicationCount >= conf.MaxReplication) begin
-						if (conf.FileOutput)
+					if (replicationCount >= conf.maxReplication) begin
+						if (conf.fileOutput)
 							$fclose(file);
 						$stop();
 					end
@@ -74,16 +74,16 @@ module ChipTest;
 					lastInstruction = chip.instr;
 					replicationCount = 0;
 				end
-				if (conf.FileOutput) begin
-					$fdisplay(file, "pc: %h", chip.cpu.pc);
+				if (conf.fileOutput) begin
+					$fdisplay(file, "pc: %h", conf.clearPcOffset ? chip.cpu.pc - InstrOffset : chip.cpu.pc);
 					$fdisplay(file, "instr: %h", chip.instr);
 					for (int i = 0; i < 32; ++i)
 						$fdisplay(file, "regfile%0d: %h", i, chip.cpu.regFile.registers[i]);
 				end
 			end
-			if (conf.PrintingTime == PrintingTime::AfterExecution) begin
+			if (conf.printingTime == PrintingTime::AfterExecution) begin
 				clock = ~clock;
-				#(conf.Period / 2);
+				#(conf.period / 2);
 			end;
 		end
 	end
