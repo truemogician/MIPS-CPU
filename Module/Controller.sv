83ea34523749bf4732b1b9465927e7ea02ba9961
true_mogician <925269713@qq.com>
2021-07-10 14:44:52 +0800

    fix: rAddr

--- Module/Controller.sv
+++ Module/Controller.sv
@@ -4,7 +4,7 @@
 `include "../Include/Parameter.sv"
 
 import AluCode::*;
-import OpType::*;
+import InstrType::*;
 import OpCode::*;
 import SpecCode::*;
 import RegimmCode::*;
@@ -13,11 +13,11 @@ import ExcCode::*;
 import Spec2Code::*;
 import Parameter::*;
 module Controller(instr,
-	opType, opCode, specCode, regimmCode, cop0Code, excCode, spec2Code, aluCode,
+	instrType, opCode, specCode, regimmCode, cop0Code, excCode, spec2Code, aluCode,
 	wAddr, rAddr1, rAddr2, immediate,
 	shamt, sext, jump, branch, load, store, lsLength, isUnsigned, mult, div, jal);
 	input	Instr instr;
-	output	OpTypeEnum opType;
+	output	InstrTypeEnum instrType;
 	output	OpCodeEnum opCode;
 	output	SpecCodeEnum specCode;
 	output	RegimmCodeEnum regimmCode;
@@ -44,14 +44,14 @@ module Controller(instr,
 	assign opCode = OpCodeEnum'(instr[31 : 26]);
 	assign specCode = opCode != OpCode::SPECIAL ? SpecCode::NONE : SpecCodeEnum'(instr[5 : 0]);
 	assign regimmCode = opCode != OpCode::REGIMM ? RegimmCode::NONE : RegimmCodeEnum'(instr[20 : 16]);
-	assign cop0Code = opCode != OpCode::COP0 ? Cop0Code::NONE : Cop0CodeEnum'(instr[25] ? instr[5 : 0] : {1'bz, instr[25 : 21]});
+	assign cop0Code = opCode != OpCode::COP0 ? Cop0Code::NONE : Cop0CodeEnum'(instr[25] ? instr[5 : 0] : {1'b0, instr[25 : 21]});
 	assign spec2Code = opCode != OpCode::SPECIAL2 ? Spec2Code::NONE : Spec2CodeEum'(instr[5 : 0]);
 	always_comb begin
 		unique case(opCode) inside
-			OpCode::SPECIAL, OpCode::SPECIAL2: opType = OpType::Register;
-			OpCode::J, OpCode::JAL: opType = OpType::Jump;
-			6'b0100??: opType = OpType::Coprocessor;
-			default: opType = OpType::Immediate;
+			OpCode::SPECIAL, OpCode::SPECIAL2: instrType = InstrType::Register;
+			OpCode::J, OpCode::JAL: instrType = InstrType::Jump;
+			6'b0100??: instrType = InstrType::Coprocessor;
+			default: instrType = InstrType::Immediate;
 		endcase
 		unique case(specCode) inside
 			SpecCode::SYSCALL: excCode = ExcCode::Sys;
@@ -61,16 +61,16 @@ module Controller(instr,
 		endcase
 	end
 	RegAddr rsAddr, rtAddr, rdAddr;
-	assign {rsAddr, rtAddr} = opType == OpType::Jump ? 'z : instr[25 : 16];
+	assign {rsAddr, rtAddr} = instrType == InstrType::Jump ? 'z : instr[25 : 16];
 	assign rdAddr = opCode == OpCode::SPECIAL || opCode == OpCode::SPECIAL2 ? instr[15 : 11] : 'z;
 	
 	assign shamt = specCode ==? 6'b0000??;	//SLL, SRL, SRA
 	assign sext = opCode ==? 6'b0010??;		//ADDI, ADDIU, SLTI, SLTIU
 	assign immediate = shamt ? `EXT(instr[10 : 6], 26)
-		: opType == OpType::Register || opType == OpType::Coprocessor ? 'z
-			: opType == OpType::Jump ? instr[25 : 0]
+		: instrType == InstrType::Register || instrType == InstrType::Coprocessor ? 'z
+			: instrType == InstrType::Jump ? instr[25 : 0]
 				: sext ? `SEXT(instr[15 : 0], 26) : `EXT(instr[15 : 0], 26);
-	assign jump = opType == OpType::Jump | specCode ==? 6'b00100?; //JR, JALR
+	assign jump = instrType == InstrType::Jump | specCode ==? 6'b00100?; //JR, JALR
 	assign branch = opCode ==? 6'b0001?? | regimmCode ==? 5'b0000?;
 	assign load = opCode ==? 6'b100???;		//LW, LH, LHU, LB, LBU
 	assign store = opCode ==? 6'b101???;		//SW, SH, SB
@@ -83,12 +83,12 @@ module Controller(instr,
 	assign lsLength = load | store ? opCode[1 : 0] : 'z;	//Word:11, Halfword:01, Byte: 00
 	assign exception = opCode =?= 6'b110??? | specCode ==? 6'b00110?;	//TRAP, SYSCALL, BREAK
 	assign jal = opCode == OpCode::JAL || specCode == SpecCode::JALR;
-	assign wAddr.hasValue = ~(store | mult | div | toHilo | branch | (jump & specCode != SpecCode::JALR) | (opType == OpType::Coprocessor & instr[25 : 21] != '0));
-	assign wAddr.value =  wAddr.hasValue ? opType == OpType::Register | spec2Code ==? 6'b10000? ? rdAddr : rtAddr : 'z;
-	assign rAddr1.hasValue = ~(shamt | fromHilo | opType == OpType::Jump | opCode == OpCode::LUI);
-	assign rAddr1 = rAddr1.hasValue ? rsAddr : 'z;
-	assign rAddr2.hasValue = store | (branch & opCode != OpCode::REGIMM) | (opType == OpType::Register & ~jump) | (opType == OpType::Coprocessor & instr[25 : 21] == 5'b00100);
-	assign rAddr2 = rAddr2.hasValue ? rtAddr : 'z;
+	assign wAddr.hasValue = ~(store | mult | div | toHilo | branch | (jump & specCode != SpecCode::JALR) | (instrType == InstrType::Coprocessor & instr[25 : 21] != '0));
+	assign wAddr.value =  wAddr.hasValue ? instrType == InstrType::Register | spec2Code ==? 6'b10000? ? rdAddr : rtAddr : '0;
+	assign rAddr1.hasValue = ~(shamt | fromHilo | instrType == InstrType::Jump | opCode == OpCode::LUI);
+	assign rAddr1.value = rAddr1.hasValue ? rsAddr : '0;
+	assign rAddr2.hasValue = store | (branch & opCode != OpCode::REGIMM) | (instrType == InstrType::Register & ~jump) | (instrType == InstrType::Coprocessor & instr[25 : 21] == 5'b00100);
+	assign rAddr2.value = rAddr2.hasValue ? rtAddr : '0;
 
 	always_comb begin : GenerateAluCode
 		if (opCode == OpCode::SPECIAL) begin
